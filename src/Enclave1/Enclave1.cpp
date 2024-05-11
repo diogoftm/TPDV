be964f91a77e0545668eebbc834691f14c718344
Tiago Silvestre <tiago.silvestre@ua.pt>
2024-05-11 09:37:40 +0100

    refactored clone

--- src/Enclave1/Enclave1.cpp
+++ src/Enclave1/Enclave1.cpp
@@ -46,6 +46,45 @@ int ecallCreateVault(const char *vaultName, const char *psw, const char *author)
     return 0;
 }
 
+int loadUnsealedData(char* unsealed_data, uint32_t sealed_size) {
+    setupVaultHeader(&_vault->header, &unsealed_data[32], &unsealed_data[64], &unsealed_data[96]);
+
+    enclavePrintf("Loading assets...\n");
+    int i = sizeof(VaultHeader);
+    while (i < sealed_size)
+    {
+        VaultAsset *newAsset = (VaultAsset *)malloc(sizeof(VaultAsset));
+
+        sgx_sha256_hash_t hash;
+        memcpy(&hash, &unsealed_data[i + 68], 32);
+
+        setupVaultAsset(newAsset, &unsealed_data[i + 32], unsealed_data[i + 64], (unsigned char *)&unsealed_data[i + 100]);
+
+        if (!ecallCheckDigest(newAsset->name, (const char *)hash))
+        {
+            return 2;
+        }
+
+        pushAsset(_vault, newAsset);
+        i += 68 + newAsset->size;
+    }
+
+    return 0;
+}
+
+static uint8_t* encryptWithAESGCM(sgx_sha256_hash_t* hash, uint8_t* src_data, uint32_t src_len) {
+    //half of the hash is being used, but no problem
+    static const char* iv = "00000000000";
+    sgx_aes_state_handle_t state;
+
+
+    sgx_aes_gcm128_enc_init((uint8_t*)hash, (uint8_t*)iv, 12, NULL, 0, &state);
+    uint8_t* dst = (uint8_t*)malloc(src_len);
+    sgx_aes_gcm128_enc_update(src_data, src_len, dst, state);
+
+    return dst;
+}
+
 int ecallOpenVault(const char *fileName, const char *psw)
 {
     _vault = (Vault *)malloc(sizeof(Vault));
@@ -59,6 +98,24 @@ int ecallOpenVault(const char *fileName, const char *psw)
     }
 
     return status;
+}
+
+
+//public int ecallOpenCipheredVault([in, string] uint8_t* data, size_t data_size, [in, string] char* psw);
+
+int ecallOpenCipheredVault(char* data, size_t data_size, char* psw) {
+
+    _vault = (Vault *)malloc(sizeof(Vault));
+    setupVault(_vault);
+
+    sgx_sha256_hash_t hash;
+
+    findPasswordHash(psw, &hash);
+
+    uint8_t* raw_data = encryptWithAESGCM(&hash, (uint8_t*)data, data_size);
+
+    loadUnsealedData((char*)raw_data, data_size);
+    return 0;
 }
 
 int ecallInsertAsset(const char *assetName, size_t assetNameSize, const uint8_t *assetData, int assetDataSize)
@@ -142,6 +199,7 @@ int ecallSaveAssetToFile(char *assetName, char *fileName)
 
     return 1;
 }
+
 
 // maybe the digest argument can be changed to a more appropiate type
 char ecallCheckDigest(const char *assetName, const char *digest)
@@ -262,27 +320,68 @@ int loadVault(const char *fileName)
         return 1;
     }
 
-    setupVaultHeader(&_vault->header, &unsealed_data[32], &unsealed_data[64], &unsealed_data[96]);
-
-    enclavePrintf("Loading assets...\n");
-    int i = sizeof(VaultHeader);
-    while (i < sealed_size)
-    {
-        VaultAsset *newAsset = (VaultAsset *)malloc(sizeof(VaultAsset));
-
-        sgx_sha256_hash_t hash;
-        memcpy(&hash, &unsealed_data[i + 68], 32);
-
-        setupVaultAsset(newAsset, &unsealed_data[i + 32], unsealed_data[i + 64], (unsigned char *)&unsealed_data[i + 100]);
-
-        if (!ecallCheckDigest(newAsset->name, (const char *)hash))
-        {
-            return 2;
-        }
-
-        pushAsset(_vault, newAsset);
-        i += 68 + newAsset->size;
-    }
-
+    loadUnsealedData(unsealed_data, sealed_size);
     return 0;
 }
+
+static int getUnsealedDataLength(VaultAsset* node) {
+    int totalAssetsSize = 0;
+    while (node != NULL)
+    {
+        totalAssetsSize += 32 + sizeof(node->name) + sizeof(node->size) + node->size + 32;
+        node = node->next;
+    }
+
+    return sizeof(VaultHeader) + totalAssetsSize;
+}
+
+//https://stackoverflow.com/questions/49769792/how-to-return-a-pointer-of-unknown-size-in-an-sgx-ecall
+int ecallGetUnsealedCipheredData_1(uint32_t* data_size) {
+    *data_size = (uint32_t)getUnsealedDataLength(_vault->asset);
+    return SGX_SUCCESS;
+}
+
+int ecallGetUnsealedCipheredData_2(uint32_t data_size, uint8_t* data) {
+    int totalAssetsSize = 0;
+    VaultAsset *node = _vault->asset;
+
+    if(data_size != getUnsealedDataLength(node)) {
+        return -1;
+    }
+
+    memcpy(data, (char *)&_vault->header, sizeof(VaultHeader));
+
+    size_t offset = sizeof(VaultHeader);
+    node = _vault->asset;
+    while (node != NULL)
+    {
+        size_t assetSize = 32 + sizeof(node->name) + sizeof(node->size) + node->size + 32;
+
+        memcpy(data + offset, node->hash, 32);
+        memcpy(data + offset + 32, node->name, sizeof(node->name));
+        memcpy(data + offset + 32 + sizeof(node->name), &node->size, sizeof(node->size));
+
+        memcpy(data + offset + 32 + sizeof(node->name) + sizeof(node->size), &node->hash, 32);
+        memcpy(data + offset + 32 + sizeof(node->name) + sizeof(node->size) + 32, node->content, node->size);
+
+        offset += assetSize;
+        node = node->next;
+    }
+
+    sgx_sha256_hash_t hash;
+    ocall_e1_print_string(_vault->header.password);
+    ocall_e1_print_string("\n");
+    findPasswordHash(_vault->header.password, &hash);
+
+
+    uint8_t* ret = encryptWithAESGCM(&hash, data, data_size);
+
+    
+    memcpy(data, ret, data_size);
+    free(ret);
+
+
+    return SGX_SUCCESS;
+}
+
+
